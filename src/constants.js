c53a197c4946c378e6b3e3f23b58349e5bdcb700
chefpkj <122695526+chefpkj@users.noreply.github.com>
2023-06-26 19:01:15 +0530

    adding phase 1

---
+++ src/constants.js
@@ -0,0 +1,3 @@
+export const newReleaseURL="https://api.spotify.com/v1/browse/new-releases";
+export const favURL="http://localhost:3000/fav";
+export const tokenURL="https://accounts.spotify.com/api/token"
